3e813ea81362b433f64359da2de666131a1d87fe
Jonathan Smith <jpls218@gmail.com>
2020-12-21 18:47:58 -0500

    more fixes

--- logic.js
+++ logic.js
@@ -1,15 +1,4 @@
-var starting = document.getElementById("start_button");
-
-starting.onclick = startQuiz;
-
-function startQuiz(){
-    var startScreen = document.querySelector("#start-screen");
-    startScreen.setAttribute("class", "hide");
-    var questions = document.removeAttribute("questions");
-    
-}  
-
-
+var startButton = document.getElementById("start_button");
 var questions = document.getElementById("questions");
 var choiceA = document.getElementById("A");
 var choiceB = document.getElementById("B");
@@ -17,32 +6,59 @@ var choiceC = document.getElementById("C");
 var choiceD = document.getElementById("D");
 var score = document.getElementById("score");
 var timeEl = document.querySelector(".time");
+var particularQuestion = document.getElementById("questions");
+var answerChoices = document.getElementById("answers");
+var secondsLeft = 100;
+var questionNumber = -1;
+var answer;
 
+startButton.onclick = startQuiz;
 
-var secondsLeft = 10;
+function startQuiz(){
+    var startScreen = document.querySelector("#start-screen");
+    startScreen.setAttribute("class", "hide");
+    document.removeAttribute("questions");
+    
+}  
 
 function setTime() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timeEl.textContent = secondsLeft + " seconds left!";
 
-    if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        sendMessage();
-      }
-
+    if (secondsLeft === 0 || questionNumber === questions.length) {
+      clearInterval(countdown);
+      setTimeout(displayScore, 500);
+    }
   }, 1000);
 }
 setTime();
+
+function makeQuestions() {
+  questionNumber++;
+  answer = questions[questionNumber].answer;
+
+  particularQuestion.textContent = questions[questionNumber].title;
+  answerChoices.innerHTML = "";
+
+  var choices = questions[questionNumber].choices;
+
+  for (var i = 0; i < choices.length; i++) {
+      var nextChoice = document.createElement("button");
+
+      nextChoice.textContent = choices[i];
+      answerBtn = answerChoices.appendChild(nextChoice);
+  }
+}
+
+
 
 function sendMessage() {
     timeEl.textContent = "Game Over!";  
 }
 
 
-function timeclock() {
-    document.getElementById
-}
+
 
 
 // Select start button
--- questions.js
+++ questions.js
@@ -1,13 +1,63 @@
-var questions = {
-    one: "How many noses do slugs have?",
-    two: "What direction do bats always fly out of a cave?",
-    three: "What material makes up a rhino's horn?",
-    four: "Which animal's brain is smaller than its eye?",
-    five: "Which of these do giraffes not have?",
-    six: "How many glasses of milk can one cow produce in its lifetime?",
-    seven: "How long does it take a slot to digest its food?",
-    eight: "How long can a snail sleep for?",
-    nine: "Which animal is the only one that can't jump?",
-    ten: "Which animal cannot vomit?"
-}
+var questions = [
+    {
+        title: "How many noses do slugs have?",
+        choices: ["1", "2", "3", "4"],
+        answer: "4"
+    },
 
+    {
+        title: "What direction do bats always fly out of a cave?",
+        choices: ["Right", "Left", "Down", "Up"],
+        answer: "Left"
+    },
+
+    {
+        title: "What material makes up a rhino's horn?",
+        choices: ["Dirt", "Clay", "Hair", "Skin"],
+        answer: "Hair"
+    },
+
+    {
+        title: "Which animal's brain is smaller than its eye?",
+        choices: ["Ostrich", "Seal", "Butteryfly", "Crocodile"],
+        answer: "Ostrich"
+    },
+
+    {
+        title: "Which of these do giraffes not have?",
+        choices: ["Legs", "Lungs", "Nose", "Vocal Chords"],
+        answer: "Vocal Chords"
+    },
+
+    {
+        title: "How many glasses of milk can one cow produce in its lifetime?",
+        choices: ["100,000", "125,000", "175,000", "200,000"],
+        answer: "200,000"
+    },
+
+    {
+        title: "How long does it take a slug to digest its food?",
+        choices: ["1 day", "1 week", "2 weeks", "1 month"],
+        answer: "2 weeks"
+    },
+
+    {
+        title: "How long can a snail sleep for?",
+        choices: ["4 hours", "13 hours", "1 day", "2 days"],
+        answer: "13 hours"
+    },
+
+    {
+        title: "Which animal is the only one that can't jump?",
+        choices: ["Elephant", "Zebra", "Yak", "Bison"],
+        answer: "Elephant"
+    },
+
+    {
+        title: "Which animal cannot vomit?",
+        choices: ["Snake", "Penguin", "Lizard", "Toad"],
+        answer: "Toad"
+    },
+
+
+];
